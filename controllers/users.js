1641268b6b9509501176e70c109cdcb502cd35f1
zienab3li <zienab2152002@gmail.com>
2025-03-11 19:50:25 +0200

    last changes

--- controllers/users.js
+++ controllers/users.js
@@ -69,7 +69,7 @@ const getUsers = async (req, res) => {
 
 const getUserById = async (req, res) => {
   const userId = req.params.id;
-  const user = await User.findById(userId).populate("posts");
+  const user = await User.findById(userId);
   if (!user) {
     throw new APIError(`user with id: ${userId} is not found`, 404);
   }
